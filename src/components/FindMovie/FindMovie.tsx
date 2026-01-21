cd656969eaadf42f74ca5ca67f6e1623814db5ca
friranger <65720979+friranger1@users.noreply.github.com>
2026-01-21 12:23:41 +0200

    Fixed link

--- src/components/FindMovie/FindMovie.tsx
+++ src/components/FindMovie/FindMovie.tsx
@@ -43,7 +43,7 @@ export const FindMovie: React.FC<Props> = ({ onAdd }) => {
             data.Poster !== 'N/A'
               ? data.Poster
               : 'https://via.placeholder.com/360x270.png?text=no%20preview',
-          imdbUrl: `https://www.imdb.com/${data.Title}/${data.imdbID}`,
+          imdbUrl: `https://www.imdb.com/title/${data.imdbID}/`,
         };
 
         setMoviePreview(normalizedMovie);
